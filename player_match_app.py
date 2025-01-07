49e324b31326857106cd9018069fde5362813874
Abdullah <asayeed@denowatts.com>
2025-01-07 11:42:29 +0600

    test some other matching

--- player_match_app.py
+++ player_match_app.py
@@ -34,9 +34,6 @@ if submit_button:
     if player_name:
         player_name_lower = player_name.lower()
         filtered_data = data[data.apply(lambda row: fuzz.partial_ratio(player_name_lower, row['Player 1'].lower()) > 80 or fuzz.partial_ratio(player_name_lower, row['Player 2'].lower()) > 80, axis=1)]
-        # filtered_data = data[(data['Player 1'].str.contains(player_name, case=False, na=False)) |
-        #                  (data['Player 2'].str.contains(player_name, case=False, na=False))]
-
         st.write(filtered_data)
     else:
         st.write("Please enter a player name.")
